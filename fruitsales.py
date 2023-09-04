1460b2d0d3a66e5aaaccf7733e5a79fc33d58cd1
Dev-Annie <85464723+Dev-Annie@users.noreply.github.com>
2023-09-03 22:05:01 -0500

    test3

--- fruitsales.py
+++ fruitsales.py
@@ -5,4 +5,4 @@ import pandas as pd
 fruit = pd.DataFrame([[30, 21]], columns=['Apples', 'Bananas'])
 fruit_sales = pd.DataFrame([[35, 21], [41, 34]], columns=['Apples', 'Bananas'],
                 index=['2017 Sales', '2018 Sales'])
-fruit.to_csv("fruit.csv")
+fruit.to_csv("c:/fruit.csv")
