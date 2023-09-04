e629923bd02cdeb9d68c6087c5290f6d57084e22
Dev-Annie <85464723+Dev-Annie@users.noreply.github.com>
2023-09-03 21:20:20 -0500

    test

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,8 @@
-# add your code here
+#import os
+import pandas as pd
+#import pytest
+
+fruit = pd.DataFrame([[30, 21]], columns=['Apples', 'Bananas'])
+fruit_sales = pd.DataFrame([[35, 21], [41, 34]], columns=['Apples', 'Bananas'],
+                index=['2017 Sales', '2018 Sales'])
+fruit.to_csv("fruit.csv")
